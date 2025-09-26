cf90b5491ace82c1a695adb1f273867544db259f
Tavin Bailey <tcbailey2@aggies.ncat.edu>
2025-09-25 22:40:28 -0400

    Pass Test Case 1: Initial game setup with required variables

---
+++ tcbailey2_assignment_3.py
@@ -0,0 +1,14 @@
+# Step 1: Foundation Setup (Test Case 1)
+# Starting stats
+student_name = "Tavin Bailey"
+current_gpa = 4.0
+study_hours = 30
+social_points = 40
+stress_level = 80
+
+# Print starting stats
+print(f"Welcome, {student_name}!") # Welcome student
+print(f"GPA: {current_gpa}") # Evaluated as float 1.0-4.0 scale
+print(f"Student Hours: {study_hours}")
+print(f"Social Points: {social_points}")
+print(f"Stress Level: {stress_level}") # Evaluated as int 0-100 scale
